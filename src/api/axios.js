27056af2298de000c6543a5e3c1c2b91ddbe9ec5
Harshada Kripal <harshadakripal@gmaol.com>
2025-04-24 22:04:11 -0500

    Fix axios baseURL and route paths

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://workersdeck-production.up.railway.app/api',
+  baseURL: 'https://workersdeck-production.up.railway.app',
   headers: {
     'Content-Type': 'application/json',
   },
