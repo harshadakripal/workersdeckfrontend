67f7627fbc876b169b28e88d0c76914a78590f08
Harshada Vijay Kripal <82953560+harshadakripal@users.noreply.github.com>
2025-12-05 14:13:31 -0600

    Update axios.js
    Changed the backend deployment platform

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://workersdeckbackend-production.up.railway.app',
+  baseURL: 'https://workersdeckbackend.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
